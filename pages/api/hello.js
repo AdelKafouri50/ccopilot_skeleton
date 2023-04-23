8161a194b5c3f2c6494302aae70f3fe89ef1bc1f
Adel Kafouri <AdelKafouri50@gmail.com>
2023-04-24 00:11:45 +0200

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import dbConnect from "../../lib/db_connect";
+
+dbConnect();
+
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
+  
 }
